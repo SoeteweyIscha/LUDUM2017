7f5c5a501582298554eb44f2100b0f9fb2abf1c4
Ischa <Soeteweyischa@gmail.com>
2017-04-23 16:38:55 +0200

    Audiofiles added + skybox

---
+++ Ludum2017/Assets/Scripts/AudioSourceControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSourceControl : MonoBehaviour
+{
+    //FIELDS
+    private AudioSource _audioSource;
+    public AudioClip[] AudioFiles;
+    private int _SoundFileSelect = 0;
+
+    //Methods
+
+    private void Start()
+    {
+        _audioSource = this.GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        if (!_audioSource.isPlaying)
+        {
+            _audioSource.PlayOneShot(AudioFiles[_SoundFileSelect], 100);
+        }
+    }
+}
