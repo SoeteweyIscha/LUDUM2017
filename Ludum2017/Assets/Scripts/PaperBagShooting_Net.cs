0a759c39d54e501b81427e569140b4124c6d1475
DESKTOP-OS1927O\gille <gilles_vc@hotmail.com>
2017-04-23 15:33:07 +0200

    Shooting

---
+++ Ludum2017/Assets/Scripts/PaperBagShooting_Net.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine;
+
+public class PaperBagShooting_Net : NetworkBehaviour {
+
+    // power of bullets
+    [SerializeField]
+    float power = 800f;
+    [SerializeField]
+    GameObject shellPrefab;
+    []
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
