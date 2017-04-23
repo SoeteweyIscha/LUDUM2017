fa40385172ac81e42e90829a41439cbe66942d8f
taho - <taho ->
2017-04-23 16:48:59 +0200

    Tree fixes

---
+++ Ludum2017/Assets/Scripts/TreeStay.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeStay : MonoBehaviour {
+
+    Rigidbody _rb;
+    Vector3 initPos;
+
+	
+	void Start () {
+        _rb = this.GetComponent<Rigidbody>();
+        initPos = transform.position.normalized;
+	}
+	
+	
+	void FixedUpdate () {
+        Vector3 Vel = _rb.velocity;
+        Vel = transform.InverseTransformVector(Vel);
+
+        Vel.x = 0;
+        Vel.z = 0;
+
+        Vel = transform.TransformVector(Vel);
+
+        _rb.velocity = Vel;
+
+        transform.position = initPos * (transform.position.magnitude);
+
+    }
+}
